c73ee70f24e852a557c64be6706b62efe434c30c
Nik <stickman7415web@gmail.com>
2018-07-23 18:25:05 -0600

    commands added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 @CellsAtWok Twitter bot
 https://twitter.com/CellsAtWok
+Made in nodeJS using mutiple npm packages.
 The code posted here is just a sample. If you try this code it wont work the way you want.
 I would recommend looking at the twit docs before using this code https://github.com/ttezel/twit
---
+++ commands.js
@@ -0,0 +1,24 @@
+/*
+This is just a continuation of the main file, just in another file on github for ease of access.
+*/
+
+//Basically stalking tweets that include @example
+var stream = T.stream('statuses/filter',{
+    stalk: '@example'
+});
+
+const anime = 'Hataraku Saibou \nCells at Work';
+const commands ='anime \nangry';
+
+stream.on('tweet', function(tweet){
+    if(tweet.text.indexOf('@example anime')>1){
+        var name = '@' + tweet.user.screen_name;
+        var nameID = tweet.id_str;
+        T.post('statuses/update',{
+            in_reply_to_status_id: nameID,
+            status: name + ' ' + anime
+        }, function(err,data,response){
+            console.log(name + ' ' + anime);
+        });
+    }
+});
